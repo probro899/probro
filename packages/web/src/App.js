3503b7401648afcb4c58e69ce8bd5212af0dea04
bhagyasah <bhagyasah4u@gmail.com>
2018-12-29 16:05:57 +0545

    rest api and eslint install

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import add from '@probro/common';
 
 class App extends Component {
+  state ={};
+
   render() {
     return (
       <div className="App">
-        this value return from common package {add(10 , 100)}
+        this value return from common package
+        {
+        add(10, 100)
+          }
       </div>
     );
   }
