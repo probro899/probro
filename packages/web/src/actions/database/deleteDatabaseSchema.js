928caed8f729217639100412dbd2983f5d24d3a4
bhagyasah <bhagyasah4u@gmail.com>
2019-09-14 11:15:05 +0545

    update and remove datase action done

---
+++ packages/web/src/actions/database/deleteDatabaseSchema.js
@@ -0,0 +1,6 @@
+import schema from '@probro/common/src/schema';
+
+export default (table, data) => async (dispatch, getState) => {
+  console.log('delete dataBaseSchema called', data, dispatch, getState());
+  dispatch(schema.remove(table, data));
+};
