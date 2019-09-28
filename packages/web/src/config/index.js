bbd29b13671a98257bdb5241c16dfc0e77a33c7b
bhagyasah <bhagyasah4u@gmail.com>
2019-09-28 10:40:43 +0545

    config chang

--- packages/web/src/config/index.js
+++ packages/web/src/config/index.js
@@ -1,7 +1,7 @@
-// const ENDPOINT = 'http://157.245.11.255:4001';
-// const SOCKETURL = '157.245.11.255';
+const ENDPOINT = 'http://157.245.11.255:4001';
+const SOCKETURL = '157.245.11.255';
 
-const ENDPOINT = 'http://localhost:4001';
-const SOCKETURL = 'localhost';
+// const ENDPOINT = 'http://localhost:4001';
+// const SOCKETURL = 'localhost';
 
 export { ENDPOINT, SOCKETURL };
