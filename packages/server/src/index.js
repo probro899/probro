624c7b8bade900c9223da17dc2fc1d3be63a5ffe
bhagyasah <bhagyasah4u@gmail.com>
2019-09-29 17:01:40 +0545

    test domain

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -15,9 +15,9 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/properclass.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/properclass.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/properclass.com/chain.pem', 'utf8');
 
 const credentials = {
   key: privateKey,
