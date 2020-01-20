969f9f98e8757836ec7a2610a2eb18b3d6a03bf7
neptech4u <bhagyasah4u@gmail.com>
2020-01-20 12:31:25 +0545

    msg notification done

--- packages/server/src/api/common/web-rtc/call-status-handler/index.js
+++ packages/server/src/api/common/web-rtc/call-status-handler/index.js
@@ -9,41 +9,44 @@ export default async function callStatusChange(data) {
   const { uid, broadCastId, broadCastType, callType, callDuration, type, connectionId } = callStatusDetails;
   const res = await db.execute(async ({ insert }) => {
     if (broadCastType === 'UserConnection') {
-      const dataTodeUpdate = {
-        type: 'Outgoing',
-        fuserId: userList[0].userId,
-        tuserId: uid,
-        duration: 0,
-        connectionId,
-        timeStamp: Date.now(),
-      };
-      const insertRes = await insert('UserMessage', dataTodeUpdate);
-      const allUserSession = session.getChannel(`${broadCastType}-${broadCastId}`);
-      const remoteSession = allUserSession.find(s => s.values.user.id === userList[0].userId);
-      const dataTobeDispatch = {
-        UserMessage: {
-          id: insertRes,
+
+      const channel = session.channel(`${broadCastType}-${broadCastId}`);
+      channel.emit('callStatus', callStatusDetails, userList);
+      if (type === 'busy') {
+        const channel = session.channel(`${broadCastType}-${broadCastId}`);
+        channel.emit('callStatus', callStatusDetails, userList);
+        const dataTodeUpdate = {
           type: 'Outgoing',
           fuserId: userList[0].userId,
           tuserId: uid,
           duration: 0,
           connectionId,
           timeStamp: Date.now(),
-          url: null,
-          message: null,
-          readStatus: null,
-        },
-      };
-      updateUserCache(dataTobeDispatch, remoteSession, 'add');
-      updateUserCache(dataTobeDispatch, session, 'add');
-      if (type === 'Busy') {
-        const channel = session.channel(`${broadCastType}-${broadCastId}`);
-        channel.emit('callStatus', callStatusDetails, userList);
+        };
+        const insertRes = await insert('UserMessage', dataTodeUpdate);
+        const allUserSession = session.getChannel(`${broadCastType}-${broadCastId}`);
+        const remoteSession = allUserSession.find(s => s.values.user.id === userList[0].userId);
+        const dataTobeDispatch = {
+          UserMessage: {
+            id: insertRes,
+            type: 'Outgoing',
+            fuserId: userList[0].userId,
+            tuserId: uid,
+            duration: 0,
+            connectionId,
+            timeStamp: Date.now(),
+            url: null,
+            message: null,
+            readStatus: null,
+          },
+        };
+        // updateUserCache(dataTobeDispatch, remoteSession, 'add');
+        updateUserCache(dataTobeDispatch, session, 'add');
       }
-      if (type === 'Ringing') {
-        const channel = session.channel(`${broadCastType}-${broadCastId}`);
-        channel.emit('callStatus', callStatusDetails, userList);
-      }
+      // if (type === 'ringing') {
+      //   const channel = session.channel(`${broadCastType}-${broadCastId}`);
+      //   channel.emit('callStatus', callStatusDetails, userList);
+      // }
     }
   });
 }
