9412cc2666fec7905d111736879412bae5cee821
probro899 <probro899@gmail.com>
2020-03-28 14:01:30 +0545

    pulling

--- packages/web/src/config/index.js
+++ packages/web/src/config/index.js
@@ -3,10 +3,10 @@
 // const PORT = 443;
 // const SOCKET_PROTOCOL = 'wss';
 
-// const ENDPOINT = 'http://localhost:4001';
-// const SOCKETURL = 'localhost';
-const ENDPOINT = 'http://192.168.100.10:4001';
-const SOCKETURL = '192.168.100.10';
+const ENDPOINT = 'http://localhost:4001';
+const SOCKETURL = 'localhost';
+// const ENDPOINT = 'http://192.168.100.10:4001';
+// const SOCKETURL = '192.168.100.10';
 
 // const ENDPOINT = 'http://192.168.1.66:4001';
 // const SOCKETURL = '192.168.1.66';
