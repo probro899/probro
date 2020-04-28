977acace733100d0ca5a88a502db411e3b593def
neptech4u <bhagyasah4u@gmail.com>
2020-04-28 20:04:38 +0545

    test

--- packages/server/src/api/common/delete/schema/helper-functions/blog/deleteBlogAll.js
+++ packages/server/src/api/common/delete/schema/helper-functions/blog/deleteBlogAll.js
@@ -1,7 +1,7 @@
 
 export default async function deleteBlogDetail(Delete, blogId) {
   // await Delete('BlogDetail', { blogId });
-  console.log('blogid in blod func', blogId);
+  // console.log('blogid in blod func', blogId);
   await Delete('BlogComment', { blogId: blogId.id });
   await Delete('BlogLike', { blogId: blogId.id });
   await Delete('Blog', blogId);
