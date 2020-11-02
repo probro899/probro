059a76b8f00cb5331ad031c6cfeac950481b66e5
Bhagya sah <bhagyasah4u@gmail.com>
2020-11-02 12:41:40 +0545

    cache removed

--- packages/server/client/src/clientConfig.js
+++ packages/server/client/src/clientConfig.js
@@ -1,24 +1,11 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import webConfig from '../../webConfig.json';
 
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'ignore',
-  },
-  query: {
-    errorPolicy: 'all',
-    fetchPolicy: 'network-only'
-  },
-};
-
 const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
     uri: `${webConfig.siteURL}/graphql`,
     credentials: 'same-origin',
   }),
-  cache: new InMemoryCache(),
-  defaultOptions,
 });
 export default client;
