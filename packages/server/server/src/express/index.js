6fa5de6e82111ef70b54da78f708bef4e99203ab
Bhagya sah <bhagyasah4u@gmail.com>
2020-11-02 13:57:41 +0545

    jpt

--- packages/server/server/src/express/index.js
+++ packages/server/server/src/express/index.js
@@ -16,18 +16,18 @@ import getErrorReport from './request-handler/getErrorReport';
 import deleteError from './request-handler/deleteError';
 
 export default function (app) {
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-  //   // Request methods you wish to allow
-  //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  //   // Set to true if you need the website to include cookies in the requests sent
-  //   // to the API (e.g. in case you use sessions)
-  //   res.setHeader('Access-Control-Allow-Credentials', true);
-  //   next();
-  // });
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
   app.post('/auth/login', login);
   app.get('/auth/reset', resetPassword);
